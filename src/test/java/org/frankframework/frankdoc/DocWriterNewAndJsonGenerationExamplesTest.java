adf16929722f5049915c03a3385c7a55aad0598e
Martijn Dirkse <martijn@integrationpartners.nl>
2021-12-09 16:39:02 +0100

    Test that same attribute value enum is defined only once

--- src/test/java/org/frankframework/frankdoc/DocWriterNewAndJsonGenerationExamplesTest.java
+++ src/test/java/org/frankframework/frankdoc/DocWriterNewAndJsonGenerationExamplesTest.java
@@ -67,6 +67,7 @@ public class DocWriterNewAndJsonGenerationExamplesTest {
 			{XsdVersion.STRICT, AttributeTypeStrategy.ALLOW_PROPERTY_REF, "reduced-digester-rules.xml", "org.frankframework.frankdoc.testtarget.entity.reference.Configuration", "entityReference.xsd", "entityReference.json"},
 			{XsdVersion.STRICT, AttributeTypeStrategy.ALLOW_PROPERTY_REF, "general-test-digester-rules.xml", "org.frankframework.frankdoc.testtarget.element.name.Master", null, "elementNames.json"},
 			{XsdVersion.STRICT, AttributeTypeStrategy.ALLOW_PROPERTY_REF, "general-test-digester-rules.xml", "org.frankframework.frankdoc.testtarget.attributeDefault.Master", "attributeDefault.xsd", null},
+			{XsdVersion.STRICT, AttributeTypeStrategy.ALLOW_PROPERTY_REF, "general-test-digester-rules.xml", "org.frankframework.frankdoc.testtarget.examples.sameEnum.only.once.Master", "enumOnlyOnce.xsd", null},
 			// Also tests tags.
 			{XsdVersion.STRICT, AttributeTypeStrategy.ALLOW_PROPERTY_REF, "general-test-digester-rules.xml", "org.frankframework.frankdoc.testtarget.examples.forwards.Master", null, "forwards.json"},
 			{XsdVersion.STRICT, AttributeTypeStrategy.ALLOW_PROPERTY_REF, "general-test-digester-rules.xml", "org.frankframework.frankdoc.testtarget.parent.without.attributes.Master", "handleParentOnlyExcludedAttributes.xsd", null}
---
+++ src/test/java/org/frankframework/frankdoc/testtarget/examples/sameEnum/only/once/Master.java
@@ -0,0 +1,9 @@
+package org.frankframework.frankdoc.testtarget.examples.sameEnum.only.once;
+
+public class Master {
+	public void setFirstAttribute(MyEnum value) {
+	}
+
+	public void setSecondAttribute(MyEnum value) {
+	}
+}
