24d065e02ab1a93ef61bb688e74e67fa8db20058
Martijn Dirkse <martijn@integrationpartners.nl>
2022-02-14 11:29:29 +0100

    MandatoryAttribute -> FrankMandatory

--- src/main/java/org/frankframework/frankdoc/model/Feature.java
+++ src/main/java/org/frankframework/frankdoc/model/Feature.java
@@ -22,7 +22,7 @@ import org.frankframework.frankdoc.wrapper.FrankEnumConstant;
 import org.frankframework.frankdoc.wrapper.FrankMethod;
 
 final class Feature {
-	static final Feature MANDATORY = new Feature("nl.nn.adapterframework.doc.MandatoryAttribute", "@ff.mandatory");
+	static final Feature MANDATORY = new Feature("nl.nn.adapterframework.doc.FrankMandatory", "@ff.mandatory");
 	static final Feature DEFAULT = new Feature("nl.nn.adapterframework.doc.DefaultValue", "@ff.default");
 	static final Feature DEPRECATED = new Feature("java.lang.Deprecated", "@deprecated");
 
