1724f9a5b49647fb71d841dd20757a5ec7f9efc1
Martijn Dirkse <martijn@integrationpartners.nl>
2022-05-19 09:02:41 +0200

    Update description of Module element and tests

--- src/main/java/org/frankframework/frankdoc/Constants.java
+++ src/main/java/org/frankframework/frankdoc/Constants.java
@@ -18,7 +18,7 @@ package org.frankframework.frankdoc;
 
 public class Constants {
 	static final String MODULE_ELEMENT_NAME = "Module";
-	static final String MODULE_ELEMENT_DESCRIPTION = "Root element for XML files that contain multiple adapters and/or jobs scheduled for periodic execution. Such an XML file is included as entity reference in another XML file. The <Module> tag does not influence the behavior of Frank configs.";
+	static final String MODULE_ELEMENT_DESCRIPTION = "Root element for XML files that contain multiple adapters and/or jobs scheduled for periodic execution. Such an XML file is included as entity reference in another XML file. The &lt;Module&gt; tag does not influence the behavior of Frank configs.";
 	public static final String IGNORE_COMPATIBILITY_MODE = "ignoreInCompatibilityMode";
 	public static final String JAVA_DOC_TAG_MANDATORY = "@ff.mandatory";
 }
