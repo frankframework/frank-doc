72e8c1978dfbf5d481aa2393f4c28305b3db82be
Sergi Philipsen <philipsen.sergi@gmail.com>
2022-09-27 13:54:23 +0200

    Fix getFrankDocVersion script

--- .github/workflows/getFrankDocVersion.sh
+++ .github/workflows/getFrankDocVersion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-grep "frankDocVersion" frank-doc/target/test-classes/build.properties | cut -d= -f2 > frankDocVersion.txt
+grep "frankDocVersion" ../../frank-doc-doclet/target/test-classes/build.properties | cut -d= -f2 > frankDocVersion.txt
 echo $(cat frankDocVersion.txt)
