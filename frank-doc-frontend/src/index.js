e8142f7ad94d88c8aafeb7c9b100b5a578cc2d59
Sergi Philipsen <philipsen.sergi@gmail.com>
2022-09-28 13:25:41 +0200

    Move the Frank!Doc frontend to this project (#134)

---
+++ frank-doc-frontend/src/index.js
@@ -0,0 +1,12 @@
+import 'angular';
+import 'angular-ui-router';
+import 'angular-sanitize';
+import 'angular-ui-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/scss/font-awesome.scss';
+import './scss/style.scss';
+
+import './js/app.js';
+import './js/controllers.js';
+import './js/directives.js';
---
+++ frank-doc-frontend/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        filename: 'js/main.[contenthash].js',
+        path: path.resolve(__dirname, 'dist/'),
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "./src/images", to: "images" },
+                { from: "./src/views", to: "views" },
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+		new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            injectType: 'linkTag'
+                        }
+                    },
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: "css/[name].[contenthash].css",
+                        }
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+        ]
+    }
+};
