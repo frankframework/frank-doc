a5fd0a88dacb81b672ab82345dc8d01edd1e9120
Sergi Philipsen <philipsen.sergi@gmail.com>
2022-09-30 10:10:57 +0200

    Add an import for the font awesome fonts and change mode to production (#136)

--- frank-doc-frontend/src/index.js
+++ frank-doc-frontend/src/index.js
@@ -5,6 +5,13 @@ import 'angular-ui-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/scss/font-awesome.scss';
+
+import 'font-awesome/fonts/fontawesome-webfont.svg';
+import 'font-awesome/fonts/fontawesome-webfont.eot';
+import 'font-awesome/fonts/fontawesome-webfont.ttf';
+import 'font-awesome/fonts/fontawesome-webfont.woff';
+import 'font-awesome/fonts/fontawesome-webfont.woff2';
+
 import './scss/style.scss';
 
 import './js/app.js';
