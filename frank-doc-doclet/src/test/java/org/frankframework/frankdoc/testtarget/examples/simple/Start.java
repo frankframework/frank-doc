2c7b5f1d13bd370f1b60a27439df09203b343d35
Martijn Dirkse <martijn.dirkse@nn.nl>
2023-01-09 11:40:11 +0100

    Have an integration test for {@value ...} and cover class description

--- frank-doc-doclet/src/test/java/org/frankframework/frankdoc/testtarget/examples/simple/Start.java
+++ frank-doc-doclet/src/test/java/org/frankframework/frankdoc/testtarget/examples/simple/Start.java
@@ -1,6 +1,13 @@
 package org.frankframework.frankdoc.testtarget.examples.simple;
 
+/**
+ * Description of Start. Value of VARIABLE is {@value #VARIABLE}.
+ * @author M66C303
+ *
+ */
 public class Start extends AbstractParentOfStart {
+	public static final String VARIABLE = "variable value";
+
 	public void setIChild(IChild child) {
 	}
 
