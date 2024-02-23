5b370bb3684bc984a9567b59ccbc60ef4e7e019a
Martijn Dirkse <martijn@integrationpartners.nl>
2024-02-23 14:48:38 +0100

    Do not use FrankDocGroup annotation to test wrapper API

--- frank-doc-doclet/src/test/java/org/frankframework/frankdoc/testtarget/doclet/interfaces/java5/annotation/ClassWithJava5Annotation.java
+++ frank-doc-doclet/src/test/java/org/frankframework/frankdoc/testtarget/doclet/interfaces/java5/annotation/ClassWithJava5Annotation.java
@@ -1,11 +1,9 @@
 package org.frankframework.frankdoc.testtarget.doclet.interfaces.java5.annotation;
 
-import org.frankframework.doc.FrankDocGroup;
-
 /**
  * @author martijn
  *
  */
-@FrankDocGroup(name = "ClassGroup")
+@MyAnnotation(name = "ClassGroup")
 public class ClassWithJava5Annotation implements InterfaceWithJavaDocTag {
 }
--- frank-doc-doclet/src/test/java/org/frankframework/frankdoc/wrapper/FrankClassJava5AnnotationTest.java
+++ frank-doc-doclet/src/test/java/org/frankframework/frankdoc/wrapper/FrankClassJava5AnnotationTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class FrankClassJava5AnnotationTest {
 	private static final String PACKAGE = "org.frankframework.frankdoc.testtarget.doclet.interfaces.java5.annotation.";
-
+	private static final String TEST_ANNOTATION = "org.frankframework.frankdoc.testtarget.doclet.interfaces.java5.annotation.MyAnnotation";
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] {
 			{"ClassWithJava5Annotation", "ClassGroup"},
@@ -31,11 +31,11 @@ public class FrankClassJava5AnnotationTest {
 		FrankClass instance = repository.findClass(PACKAGE + queriedClass);
 		// TODO: Will rename this method and give it Java annotation class name as argument.
 		// This has to be done for similar methods in FrankMethod as well.
-		FrankAnnotation actualGroupAnnotation = instance.getAnnotationIncludingInherited(JAVADOC_GROUP_ANNOTATION);
+		FrankAnnotation actualAnnotation = instance.getAnnotationIncludingInherited(TEST_ANNOTATION);
 		if(expectedValue == null) {
-			assertNull(actualGroupAnnotation);
+			assertNull(actualAnnotation);
 		} else {
-			assertEquals(expectedValue, actualGroupAnnotation.getValueOf("name"));
+			assertEquals(expectedValue, actualAnnotation.getValueOf("name"));
 		}
 	}
 
