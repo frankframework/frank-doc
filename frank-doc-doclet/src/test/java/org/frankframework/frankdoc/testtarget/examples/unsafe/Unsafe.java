df71f8b7948c195b40abd18de7887f3b3290b585
Thomas P <thomaspj2003@gmail.com>
2024-08-30 10:43:39 +0200

    Add a test for the @Unsafe annotation.

---
+++ frank-doc-doclet/src/test/java/org/frankframework/frankdoc/testtarget/examples/unsafe/Unsafe.java
@@ -0,0 +1,12 @@
+package org.frankframework.frankdoc.testtarget.examples.unsafe;
+
+public class Unsafe {
+
+	@org.frankframework.doc.Unsafe
+	public void setUnsafeAttribute(String attribute) {
+	}
+
+	public void setSafeAttribute(String attribute) {
+	}
+
+}
