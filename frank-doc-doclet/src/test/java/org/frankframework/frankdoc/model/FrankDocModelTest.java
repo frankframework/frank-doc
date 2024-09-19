fd6bf615d4989ef13699903c7967962f1e264a13
Thomas P <thomaspj2003@gmail.com>
2024-09-19 13:39:33 +0200

    Revert bad test removal.

--- frank-doc-doclet/src/test/java/org/frankframework/frankdoc/model/FrankDocModelTest.java
+++ frank-doc-doclet/src/test/java/org/frankframework/frankdoc/model/FrankDocModelTest.java
@@ -426,19 +426,19 @@ public class FrankDocModelTest {
 		assertEquals("setReferToInheritedDescription description", actual.getDescription());
 	}
 
-//	@Test
-//	public void testReferToMissingMethod() throws Exception {
-//		TestAppender appender = TestAppender.newBuilder().build();
-//		TestAppender.addToRootLogger(appender);
-//		try {
-//			Reference reference = new Reference(classRepository);
-//			FrankMethod targetMethod = Arrays.stream(classRepository.findClass(REFERRER).getDeclaredMethods()).filter(frankMethod -> frankMethod.getName().equals("doesNotExistsMethod")).findFirst().get();
-//			reference.valueOf(targetMethod);
-//			appender.assertLogged("Referred method [org.frankframework.frankdoc.testtarget.ibisdocref.ChildTargetParameterized] does not exist, as specified at location: [Referrer.doesNotExistsMethod]");
-//		} finally {
-//			TestAppender.removeAppender(appender);
-//		}
-//	}
+	@Test
+	public void testReferToMissingMethod() throws Exception {
+		TestAppender appender = TestAppender.newBuilder().build();
+		TestAppender.addToRootLogger(appender);
+		try {
+			Reference reference = new Reference(classRepository);
+			FrankMethod targetMethod = Arrays.stream(classRepository.findClass(REFERRER).getDeclaredMethods()).filter(frankMethod -> frankMethod.getName().equals("doesNotExistsMethod")).findFirst().get();
+			reference.valueOf(targetMethod);
+			appender.assertLogged("Referred method [org.frankframework.frankdoc.testtarget.ibisdocref.ChildTargetParameterized] does not exist, as specified at location: [Referrer.doesNotExistsMethod]");
+		} finally {
+			TestAppender.removeAppender(appender);
+		}
+	}
 
 	@Test
 	public void testReferToWithParameterizedTargetClass() throws Exception {
