be0b5ac224e42fa147a7d10d4c96abe766ca7d96
Vivy <4380412+Matthbo@users.noreply.github.com>
2024-10-22 19:14:32 +0200

    Add basics of index page

--- frank-doc-frontend/src/app/app.routes.ts
+++ frank-doc-frontend/src/app/app.routes.ts
@@ -1,10 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { IndexComponent } from './views/index/index.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home',
+  },
+  {
+    path: 'index',
+    component: IndexComponent,
+    title: 'Index',
+  },
+  /*{
+    path: 'element',
+    component: ElemementComponent,
+  },*/
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
--- frank-doc-frontend/src/app/components/header/header.component.ts
+++ frank-doc-frontend/src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { IconFfLogoSmallComponent } from '../../icons/icon-reference/icon-ff-logo-small.component';
 import { IconDownloadComponent } from '../../icons/icon-download/icon-download.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IconDarkmodeComponent } from '../../icons/icon-darkmode/icon-darkmode.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [IconFfLogoSmallComponent, IconDownloadComponent],
+  imports: [IconFfLogoSmallComponent, IconDownloadComponent, RouterLink, RouterLinkActive, IconDarkmodeComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
