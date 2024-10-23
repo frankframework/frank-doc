200297449394d0af51f84773e78495813e5b9dea
Vivy <4380412+Matthbo@users.noreply.github.com>
2024-10-23 16:49:55 +0200

    Filter creation & assignments, index page improvements

--- frank-doc-frontend/src/app/views/index/index.component.ts
+++ frank-doc-frontend/src/app/views/index/index.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, Signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { AppService, Filter } from '../../app.service';
+import { ChipComponent } from '@frankframework/angular-components';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ChipComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss',
 })
-export class IndexComponent implements OnInit {
-  protected filters: Filter[] = this.appService.filters();
+export class IndexComponent {
+  protected filters: Signal<Filter[]> = computed(() => this.appService.filters());
   constructor(private appService: AppService) {}
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      console.log(this.filters);
-    });
-  }
 }
