af94ba8b8f2c7ac6a90ae5b11abd60d485b19b88
Vivy <4380412+Matthbo@users.noreply.github.com>
2024-11-04 17:18:09 +0100

    Add old url scheme, improve finding element & add component options

--- frank-doc-frontend/src/app/views/details/details-element/details-element.component.ts
+++ frank-doc-frontend/src/app/views/details/details-element/details-element.component.ts
@@ -1,26 +1,40 @@
-import { Component, Input } from '@angular/core';
-import { ChipComponent } from '@frankframework/angular-components';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { AlertComponent, ChipComponent } from '@frankframework/angular-components';
 import { JavadocPipe } from '../../../components/javadoc.pipe';
 import { KeyValuePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { Element, FrankDoc } from '../../../frankdoc.types';
+import { Attribute, Element, FrankDoc } from '../../../frankdoc.types';
 import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-details-element',
   standalone: true,
-  imports: [ChipComponent, JavadocPipe, KeyValuePipe, RouterLink],
+  imports: [ChipComponent, JavadocPipe, KeyValuePipe, RouterLink, AlertComponent],
   templateUrl: './details-element.component.html',
   styleUrl: './details-element.component.scss',
 })
-export class DetailsElementComponent {
+export class DetailsElementComponent implements OnChanges {
   @Input({ required: true }) element!: Element | null;
   @Input({ required: true }) frankDocElements!: FrankDoc['elements'] | null;
 
+  protected attributesRequired: Attribute[] = [];
+  protected attributesOptional: Attribute[] = [];
+
   private filterColours: string[] = ['#CD55EB', '#037CD4', '#00B31D'];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['element'] && this.element?.attributes) {
+      this.attributesRequired = this.element?.attributes.filter((attribute) => attribute.mandatory === true) ?? [];
+      this.attributesOptional = this.element?.attributes.filter((attribute) => !attribute.mandatory) ?? [];
+    }
+  }
 
   getLabelColor(index: number): string {
     return this.filterColours[index % this.filterColours.length];
+  }
+
+  githubUrlOf(name: string): string {
+    return `${environment.githubWikiBaseUrl}/${name}`;
   }
 
   javaDocUrlOf(fullName: string): string | null {
--- frank-doc-frontend/src/app/views/details/details.component.ts
+++ frank-doc-frontend/src/app/views/details/details.component.ts
@@ -8,7 +8,7 @@ import { JavadocPipe } from '../../components/javadoc.pipe';
 import { DetailsElementComponent } from './details-element/details-element.component';
 
 type ElementFilterProperties = {
-  filterName: string;
+  filterName?: string;
   labelName: string;
   elementName: string;
 };
@@ -37,18 +37,22 @@ export class DetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
+      const frankDocElements = this.frankDocElements();
       const fullName: string | undefined = params['fullname'];
       if (fullName) {
         this.fullElementName = fullName;
+        if (frankDocElements) this.getElementByFullName(frankDocElements, fullName);
+        return;
       }
 
       const filterName: string | undefined = params['filter'];
       const labelName: string | undefined = params['label'];
       const elementName: string | undefined = params['element'];
 
-      if (!filterName || !labelName || !elementName) {
+      if (!labelName || !elementName) {
         return;
       }
+
       this.elementFilterProperties = { filterName, labelName, elementName };
       const elements = this.frankDocElements();
       if (elements) this.getElementByFilterLabelNames(elements, this.elementFilterProperties);
@@ -57,7 +61,7 @@ export class DetailsComponent implements OnInit {
 
   private findElement(frankDocElements: FrankDoc['elements'] | null): FrankDoc['elements'][string] | null {
     if (!frankDocElements) return null;
-    if (this.fullElementName) return this.getElementByFullName(frankDocElements);
+    if (this.fullElementName) return this.getElementByFullName(frankDocElements, this.fullElementName);
     if (!this.elementFilterProperties) return null;
     return this.getElementByFilterLabelNames(frankDocElements, this.elementFilterProperties);
   }
@@ -68,14 +72,29 @@ export class DetailsComponent implements OnInit {
   ): FrankDoc['elements'][string] | null {
     const elements = Object.values(frankDocElements);
     const { filterName, labelName, elementName } = elementFilterProperties;
+
+    if (!filterName) {
+      return this.getElementByShortName(elements, elementName);
+    }
+
     const filteredElement = elements.find((element) => {
       return element.name === elementName && element.labels?.[filterName]?.includes(labelName);
     });
+    console.log(filteredElement);
     return filteredElement ?? null;
   }
 
-  private getElementByFullName(frankDocElements: FrankDoc['elements'] | null): FrankDoc['elements'][string] | null {
-    if (!frankDocElements || !this.fullElementName) return null;
-    return frankDocElements[this.fullElementName] ?? null;
+  private getElementByShortName(frankDocElements: Element[], shortName: string): Element | null {
+    // return frankDocElements.find((element) => element.name === shortName) ?? null;
+    const filteredElement = frankDocElements.find((element) => element.name === shortName);
+    console.log(filteredElement);
+    return filteredElement ?? null;
+  }
+
+  private getElementByFullName(frankDocElements: FrankDoc['elements'], fullName: string): Element | null {
+    // return frankDocElements[fullName] ?? null;
+    const filteredElement = frankDocElements[fullName];
+    console.log(filteredElement);
+    return filteredElement ?? null;
   }
 }
