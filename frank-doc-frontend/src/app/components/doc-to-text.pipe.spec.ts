5be090b65e7459161c6d895741865fce6e6a0135
Vivy <4380412+Matthbo@users.noreply.github.com>
2024-11-05 19:30:32 +0100

    A mess to try and get javadoc description properly working

---
+++ frank-doc-frontend/src/app/components/doc-to-text.pipe.spec.ts
@@ -0,0 +1,108 @@
+import { DocToTextPipe } from './doc-to-text.pipe';
+import { TestBed } from '@angular/core/testing';
+import { AppService } from '../app.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+
+describe('DocToTextPipe', () => {
+  // let appService: AppService;
+  // let domSanitizer: DomSanitizer;
+  let pipe: DocToTextPipe;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [
+        AppService,
+        {
+          provide: DomSanitizer,
+          useValue: {
+            bypassSecurityTrustHtml: (html: string): string => html,
+          },
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+      ],
+    });
+    // appService = TestBed.inject(AppService);
+    // domSanitizer = TestBed.inject(DomSanitizer);
+    pipe = new DocToTextPipe();
+  });
+
+  it('create an instance', () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  /*it('transforms markdown link to html link', () => {
+    expect(pipe.transform('A [link](http://example.com) to an example website', {})).toEqual(
+      'A <a target="_blank" href="http://example.com" alt="link">link</a> to an example website',
+    );
+  });
+
+  it('transforms javadoc quotes to normal quotes', () => {
+    expect(pipe.transform('A javadoc \\"quote\\"', {})).toEqual('A javadoc "quote"');
+  });
+
+  it('transforms javadoc invalid link to html text', () => {
+    expect(pipe.transform('A javadoc {@link http://example.com link} to an example website', {})).toEqual(
+      'A javadoc link to an example website',
+    );
+  });
+
+  it('transforms basic javadoc valid link to html link', () => {
+    const elements: FrankDoc['elements'] = {
+      'org.frankframework.pipes.Json2XmlValidator': {
+        name: 'Json2XmlValidator',
+        fullName: 'org.frankframework.pipes.Json2XmlValidator',
+        elementNames: [],
+      },
+    };
+    expect(pipe.transform('A javadoc {@link Json2XmlValidator}', elements)).toEqual(
+      'A javadoc <a href="#/All/Json2XmlValidator">Json2XmlValidator</a>',
+    );
+  });
+
+  it('transforms javadoc link to class with label to html link', () => {
+    const elements: FrankDoc['elements'] = {
+      'org.frankframework.pipes.PipeLineSession': {
+        name: 'PipeLineSession',
+        fullName: 'org.frankframework.pipes.PipeLineSession',
+        elementNames: [],
+      },
+    };
+    expect(pipe.transform('{@link PipeLineSession pipeLineSession}', elements)).toEqual(
+      '<a href="#/All/PipeLineSession">pipeLineSession</a>',
+    );
+  });
+
+  it('transforms invalid javadoc link to class with method to html link', () => {
+    const elements: FrankDoc['elements'] = {};
+    expect(pipe.transform('{@link IPipe#configure()}', elements)).toEqual('IPipe.configure()');
+  });
+
+  it('transforms valid javadoc link to class with method to html link', () => {
+    const elements: FrankDoc['elements'] = {
+      'org.frankframework.pipes.IPipe': {
+        name: 'IPipe',
+        fullName: 'org.frankframework.pipes.IPipe',
+        elementNames: [],
+      },
+    };
+    expect(pipe.transform('{@link IPipe#configure()}', elements)).toEqual(
+      '<a href="#/All/IPipe">IPipe.configure()</a>',
+    );
+  });
+
+  it('transforms javadoc link to same class method to html link', () => {
+    const elements: FrankDoc['elements'] = {};
+    expect(pipe.transform('{@link #doPipe(Message, PipeLineSession) doPipe}', elements)).toEqual('doPipe');
+  });
+
+  it('transforms javadoc links without parentheses', () => {
+    const elements: FrankDoc['elements'] = {};
+    expect(
+      pipe.transform('{@link #setDestinationType destinationType} = {@link DestinationType#TOPIC TOPIC}', elements),
+    ).toEqual('destinationType = TOPIC');
+  });*/
+});
---
+++ frank-doc-frontend/src/app/components/javadoc-transformer.directive.spec.ts
@@ -0,0 +1,8 @@
+import { JavadocTransformerDirective } from './javadoc-transformer.directive';
+
+describe('JavadocTransformerDirective', () => {
+  it('should create an instance', () => {
+    const directive = new JavadocTransformerDirective();
+    expect(directive).toBeTruthy();
+  });
+});
