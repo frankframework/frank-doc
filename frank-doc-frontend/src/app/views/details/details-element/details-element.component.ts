53634a6725912a6216dacb71b724905d775fb47f
Vivy <4380412+Matthbo@users.noreply.github.com>
2024-11-28 18:35:29 +0100

    Preparations of handling inherited properties

--- frank-doc-frontend/src/app/views/details/details-element/details-element.component.ts
+++ frank-doc-frontend/src/app/views/details/details-element/details-element.component.ts
@@ -2,10 +2,21 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { AlertComponent, ChipComponent } from '@frankframework/angular-components';
 import { KeyValuePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { Attribute, Element, FrankDoc } from '../../../frankdoc.types';
+import { Attribute, Element, FrankDoc, ParsedTag } from '../../../frankdoc.types';
 import { environment } from '../../../../environments/environment';
 import { JavadocTransformDirective } from '../../../components/javadoc-transform.directive';
 import { CollapseDirective } from '../../../components/collapse.directive';
+
+type InheritedParentElementProperties<T> = {
+  parentElementName: string;
+  properties: T[];
+};
+
+type InheritedProperties = {
+  attributes: InheritedParentElementProperties<Attribute>[];
+  parameters: InheritedParentElementProperties<ParsedTag>[];
+  forwards: InheritedParentElementProperties<ParsedTag>[];
+};
 
 @Component({
   selector: 'app-details-element',
@@ -20,29 +31,44 @@ export class DetailsElementComponent implements OnChanges {
 
   protected attributesRequired: Attribute[] = [];
   protected attributesOptional: Attribute[] = [];
+  protected inheritedProperties: InheritedProperties = {
+    attributes: [],
+    parameters: [],
+    forwards: [],
+  };
 
   private filterColours: string[] = ['#CD55EB', '#037CD4', '#00B31D'];
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['element'] && this.element?.attributes) {
-      this.attributesRequired = this.element?.attributes.filter((attribute) => attribute.mandatory === true) ?? [];
-      this.attributesOptional = this.element?.attributes.filter((attribute) => !attribute.mandatory) ?? [];
+    if (changes['element']) {
+      if (this.element?.attributes) {
+        this.attributesRequired = this.element?.attributes.filter((attribute) => attribute.mandatory === true) ?? [];
+        this.attributesOptional = this.element?.attributes.filter((attribute) => !attribute.mandatory) ?? [];
+      }
+      if (this.element?.parent) {
+        this.getInheritedProperties(this.element.parent);
+      }
     }
   }
 
-  getLabelColor(index: number): string {
+  protected getLabelColor(index: number): string {
     return this.filterColours[index % this.filterColours.length];
   }
 
-  githubUrlOf(name: string): string {
+  protected githubUrlOf(name: string): string {
     return `${environment.githubWikiBaseUrl}/${name}`;
   }
 
-  javaDocUrlOf(fullName: string): string | null {
+  protected javaDocUrlOf(fullName: string): string | null {
     return fullName.includes('.')
       ? `${environment.javadocBaseUrl}/${fullName.replaceAll('.', '/')}.html`
       : // We only have a JavaDoc URL if we have an element with a Java class. The
         // exception we handle here is <Module>.
         null;
+  }
+
+  private getInheritedProperties(elementIndex: string): void {
+    elementIndex;
+    // TODO get inherited properties from parent element recursively
   }
 }
