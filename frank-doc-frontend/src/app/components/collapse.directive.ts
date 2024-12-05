9e1499c4f7554ff56d141dbcdd51cd49f2cbf892
Vivy <4380412+Matthbo@users.noreply.github.com>
2024-12-05 17:18:36 +0100

    Improve collapsables

--- frank-doc-frontend/src/app/components/collapse.directive.ts
+++ frank-doc-frontend/src/app/components/collapse.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, Input, booleanAttribute } from '@angular/core';
+import { Directive, HostListener, Input, booleanAttribute, Output, EventEmitter } from '@angular/core';
 
 @Directive({
   selector: '[collapse]',
@@ -7,7 +7,8 @@ import { Directive, HostListener, Input, booleanAttribute } from '@angular/core'
 export class CollapseDirective {
   @Input({ required: true }) collapse!: HTMLElement;
   @Input({ transform: booleanAttribute }) collapsed: boolean = false;
-  @Input() animationSpeed: number = 200;
+  @Input() animationSpeed: number = 150;
+  @Output() collapsedChange = new EventEmitter<boolean>();
 
   @HostListener('click')
   onClick(): void {
@@ -17,24 +18,27 @@ export class CollapseDirective {
     } else {
       this.expandElement();
     }
+    this.collapsedChange.emit(this.collapsed);
   }
 
   collapseElement(): void {
-    this.collapse.style.height = `${this.collapse.clientHeight}px`;
     this.collapse.style.overflowY = 'hidden';
-    this.collapse.animate({ height: '0px' }, this.animationSpeed).finished.then(() => {
-      this.collapse.style.height = '0px';
-      this.collapse.style.overflowY = '';
-      this.collapse.style.overflow = 'hidden';
-    });
+    this.collapse
+      .animate({ height: [`${this.collapse.clientHeight}px`, '0px'] }, this.animationSpeed)
+      .finished.then(() => {
+        this.collapse.style.height = '0px';
+        this.collapse.style.overflowY = '';
+        this.collapse.style.overflow = 'hidden';
+      });
   }
 
   expandElement(): void {
-    this.collapse.style.height = ``;
-    this.collapse.style.overflow = '';
+    this.collapse.style.height = '';
     this.collapse.style.overflowY = 'hidden';
-    this.collapse.animate({ height: `${this.collapse.clientHeight}px` }, this.animationSpeed).finished.then(() => {
-      this.collapse.removeAttribute('style');
-    });
+    this.collapse
+      .animate({ height: ['0px', `${this.collapse.clientHeight}px`] }, this.animationSpeed)
+      .finished.then(() => {
+        this.collapse.removeAttribute('style');
+      });
   }
 }
--- frank-doc-frontend/src/app/views/index/index.component.ts
+++ frank-doc-frontend/src/app/views/index/index.component.ts
@@ -1,16 +1,20 @@
-import { Component, computed, Signal } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AppService, Filter } from '../../app.service';
 import { ChipComponent } from '@frankframework/angular-components';
+import { IconCaretComponent } from '../../icons/icon-caret-down/icon-caret.component';
+import { CollapseDirective } from '../../components/collapse.directive';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [RouterLink, ChipComponent],
+  imports: [RouterLink, ChipComponent, IconCaretComponent, CollapseDirective],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss',
 })
 export class IndexComponent {
+  protected collapsedFilters: Record<string, boolean> = {};
+
+  private appService: AppService = inject(AppService);
   protected filters: Signal<Filter[]> = computed(() => this.appService.filters());
-  constructor(private appService: AppService) {}
 }
