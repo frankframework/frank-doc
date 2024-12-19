954a5acf662f5f271b083443f6e42bb069689398
Vivy <4380412+Matthbo@users.noreply.github.com>
2024-12-19 15:29:51 +0100

    Add easing on collapse-expand animation

--- frank-doc-frontend/src/app/components/collapse.directive.ts
+++ frank-doc-frontend/src/app/components/collapse.directive.ts
@@ -7,7 +7,7 @@ import { Directive, HostListener, Input, booleanAttribute, Output, EventEmitter 
 export class CollapseDirective {
   @Input({ required: true }) collapse!: HTMLElement;
   @Input({ transform: booleanAttribute }) collapsed: boolean = false;
-  @Input() animationSpeed: number = 150;
+  @Input() animationSpeed: number = 300;
   @Output() collapsedChange = new EventEmitter<boolean>();
 
   private collapseAnimation: Animation | null = null;
@@ -33,7 +33,10 @@ export class CollapseDirective {
 
   collapseElement(): void {
     this.collapse.classList.add('transforming');
-    this.collapseAnimation = this.collapse.animate({ height: [`${this.clientHeight}px`, '0px'] }, this.animationSpeed);
+    this.collapseAnimation = this.collapse.animate(
+      { height: [`${this.clientHeight}px`, '0px'] },
+      { duration: this.animationSpeed, easing: 'ease-in-out' },
+    );
     this.collapseAnimation.finished
       .then(() => {
         this.collapse.classList.add('collapsed');
@@ -45,7 +48,10 @@ export class CollapseDirective {
   }
 
   expandElement(): void {
-    this.collapseAnimation = this.collapse.animate({ height: ['0px', `${this.clientHeight}px`] }, this.animationSpeed);
+    this.collapseAnimation = this.collapse.animate(
+      { height: ['0px', `${this.clientHeight}px`] },
+      { duration: this.animationSpeed, easing: 'ease-in-out' },
+    );
     this.collapseAnimation.finished
       .then(() => {
         this.collapse.classList.remove('collapsed');
