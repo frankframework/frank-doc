9436abae0d8a5c3e95eefb52be2f4366774ee236
Vivy <4380412+Matthbo@users.noreply.github.com>
2025-01-20 11:03:37 +0100

    Enable scrollPositionRestoration for router navigation (#264)

--- frank-doc-frontend/src/app/app.config.ts
+++ frank-doc-frontend/src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation()), provideHttpClient()],
+  providers: [
+    provideRouter(routes, withHashLocation(), withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })),
+    provideHttpClient(),
+  ],
 };
