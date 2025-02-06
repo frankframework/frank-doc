5335849f8152defb94e75dbceaa529f699b22afe
Vivy <4380412+Matthbo@users.noreply.github.com>
2025-02-06 18:09:17 +0100

    Navigate to element on title or description click

--- frank-doc-frontend/src/app/views/home/home-component-list/home-component-list.component.ts
+++ frank-doc-frontend/src/app/views/home/home-component-list/home-component-list.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, Input } from '@angular/core';
 import { ButtonComponent } from '@frankframework/angular-components';
 import { FuseResult } from 'fuse.js';
 import { Element } from '../../../frankdoc.types';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { AppService } from '../../../app.service';
 import { TruncatePipe } from '../../../components/truncate.pipe';
 import { environment } from '../../../../environments/environment';
@@ -10,7 +10,7 @@ import { environment } from '../../../../environments/environment';
 @Component({
   selector: 'app-home-component-list',
   standalone: true,
-  imports: [ButtonComponent, RouterLink, TruncatePipe],
+  imports: [ButtonComponent, TruncatePipe],
   templateUrl: './home-component-list.component.html',
   styleUrl: './home-component-list.component.scss',
 })
@@ -20,7 +20,15 @@ export class HomeComponentListComponent {
   protected relatedComponents: FuseResult<Element>[] = [];
   protected showRelated: boolean = environment.relatedSearchResults;
 
+  private router: Router = inject(Router);
   private appService: AppService = inject(AppService);
   protected getFirstFilter = this.appService.getFirstFilter;
   protected getFirstLabel = this.appService.getFirstLabel;
+
+  protected navigateToElement(element: Element): void {
+    const route = element.labels
+      ? ['/', this.getFirstFilter(element.labels), this.getFirstLabel(element.labels), element.name]
+      : ['/', element.fullName];
+    this.router.navigate(route);
+  }
 }
