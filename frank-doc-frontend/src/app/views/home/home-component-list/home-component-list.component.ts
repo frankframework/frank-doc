606459c5c62ae17aecaadd453385eb15823ceedb
Vivy <4380412+Matthbo@users.noreply.github.com>
2025-02-24 10:39:32 +0100

    Navigate to element on title or description click (#300)
    * Navigate to element on title or description click
    
    * Better hover effect over cards
    
    * Fix pointer cursor css on card hover

--- frank-doc-frontend/src/app/views/home/home-component-list/home-component-list.component.ts
+++ frank-doc-frontend/src/app/views/home/home-component-list/home-component-list.component.ts
@@ -2,15 +2,16 @@ import { Component, inject, Input } from '@angular/core';
 import { ButtonComponent } from '@frankframework/angular-components';
 import { FuseResult } from 'fuse.js';
 import { Element } from '../../../frankdoc.types';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { AppService } from '../../../app.service';
 import { TruncatePipe } from '../../../components/truncate.pipe';
 import { environment } from '../../../../environments/environment';
 import { JavadocTransformDirective } from '../../../components/javadoc-transform.directive';
+import { CardDirective } from './card.directive';
 
 @Component({
   selector: 'app-home-component-list',
-  imports: [ButtonComponent, RouterLink, TruncatePipe, JavadocTransformDirective],
+  imports: [ButtonComponent, TruncatePipe, JavadocTransformDirective, CardDirective],
   templateUrl: './home-component-list.component.html',
   styleUrl: './home-component-list.component.scss',
 })
@@ -21,7 +22,34 @@ export class HomeComponentListComponent {
   protected relatedComponents: FuseResult<Element>[] = [];
   protected showRelated: boolean = environment.relatedSearchResults;
 
+  private router: Router = inject(Router);
   private appService: AppService = inject(AppService);
   protected getFirstFilter = this.appService.getFirstFilter;
   protected getFirstLabel = this.appService.getFirstLabel;
+
+  protected navigateToElement(element: Element): void {
+    const route = element.labels
+      ? ['/', this.getFirstFilter(element.labels), this.getFirstLabel(element.labels), element.name]
+      : ['/', element.fullName];
+    this.router.navigate(route);
+  }
+
+  protected setActiveHover(card: HTMLElement, active: boolean): void {
+    try {
+      const cardElement = card as unknown as CardDirective;
+      cardElement.activeHover = active;
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+
+  protected checkActiveHover(card: HTMLElement): boolean {
+    try {
+      const cardElement = card as unknown as CardDirective;
+      return cardElement.activeHover;
+    } catch (error) {
+      console.warn(error);
+      return false;
+    }
+  }
 }
