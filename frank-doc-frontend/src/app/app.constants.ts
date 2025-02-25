c824f4bf3125bf1d3e7b70bec32b31a5179fa0ef
Vivy <4380412+Matthbo@users.noreply.github.com>
2025-02-25 13:37:23 +0100

    Improve Fuse search options

--- frank-doc-frontend/src/app/app.constants.ts
+++ frank-doc-frontend/src/app/app.constants.ts
@@ -17,15 +17,31 @@ export const fuseOptions: IFuseOptions<Element> = {
       name: 'elementNames', // default weight is 1.0
     },
     {
+      name: 'description',
+      weight: 0.5,
+    },
+    {
       name: 'labels.FrankDocGroup',
     },
     {
-      name: 'attributes',
+      name: 'attributes.name',
+      weight: 1.2,
+    },
+    {
+      name: 'attributes.description',
       weight: 0.7,
     },
     {
-      name: 'description',
-      weight: 0.5,
+      name: 'children.roleName',
+      weight: 0.7,
+    },
+    {
+      name: 'forwards.name',
+      weight: 0.7,
+    },
+    {
+      name: 'parameters.name',
+      weight: 0.7,
     },
   ],
   includeScore: true,
