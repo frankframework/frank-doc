21e1d617e62ac1dfed53e97aff4d4f2d48337a05
Vivy <4380412+Matthbo@users.noreply.github.com>
2025-04-02 17:06:50 +0200

    Fix karma test

--- frank-doc-frontend/src/app/views/credential-providers/credential-providers.component.spec.ts
+++ frank-doc-frontend/src/app/views/credential-providers/credential-providers.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CredentialProvidersComponent } from './credential-providers.component';
+import { AppService } from '../../app.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CredentialProvidersComponent', () => {
   let component: CredentialProvidersComponent;
@@ -9,6 +12,7 @@ describe('CredentialProvidersComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CredentialProvidersComponent],
+      providers: [AppService, provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CredentialProvidersComponent);
