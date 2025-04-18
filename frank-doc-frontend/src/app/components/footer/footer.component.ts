869b9a61b8ddad0e04e7cdbd65255217dc1742cf
Sergi Philipsen <philipsen.sergi@gmail.com>
2025-04-18 14:09:35 +0200

    refactor: move XSD download button from header to footer (#388)
    * refactor: move XSD download button from header to footer
    
    Remove the download XSD button and related code from the header component,
    including its styles and downloadXSD() method. Add the download XSD button
    to the footer component instead, implementing the downloadXSD() method there.
    
    This change declutters the header by removing an underused action and places
    the download functionality in the footer where it is more accessible and
    contextually appropriate. It also cleans up unused styles and imports in the
    header component.
    
    * fix: center footer icons

--- frank-doc-frontend/src/app/components/footer/footer.component.ts
+++ frank-doc-frontend/src/app/components/footer/footer.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { IconFfLogoFullComponent } from '../../icons/icon-ff-logo-full/icon-ff-logo-full.component';
 import { IconNavigateToExternalComponent } from '../../icons/icon-navigate-to-external/icon-navigate-to-external.component';
+import {IconDownloadComponent} from "../../icons/icon-download/icon-download.component";
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-footer',
-  imports: [IconFfLogoFullComponent, IconNavigateToExternalComponent],
+  imports: [IconFfLogoFullComponent, IconNavigateToExternalComponent, IconDownloadComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
   protected year = new Date().getFullYear();
+
+  downloadXSD(): void {
+    const downloadUrl = environment.xsdUrl;
+
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', downloadUrl);
+    link.setAttribute('download', '');
+    document.body.append(link);
+    link.click();
+    link.remove();
+  }
 }
