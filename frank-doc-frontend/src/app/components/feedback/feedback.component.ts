22e8bf1ba5de62fb0df7e8a6c9373e3ea381a8d7
Sergi Philipsen <philipsen.sergi@gmail.com>
2025-04-29 14:54:15 +0200

    Add feedback button and style code blocks

---
+++ frank-doc-frontend/src/app/components/feedback/feedback.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { IconDocumentAddComponent } from '../../icons/icon-document-add/icon-document-add.component';
+
+@Component({
+  selector: 'app-feedback',
+  imports: [IconDocumentAddComponent],
+  templateUrl: './feedback.component.html',
+  styleUrl: './feedback.component.scss',
+})
+export class FeedbackComponent {}
--- frank-doc-frontend/src/app/views/index/index.component.ts
+++ frank-doc-frontend/src/app/views/index/index.component.ts
@@ -4,20 +4,17 @@ import { AppService, Filter } from '../../app.service';
 import { ChipComponent } from '@frankframework/angular-components';
 import { IconCaretComponent } from '../../icons/icon-caret-down/icon-caret.component';
 import { CollapseDirective } from '../../components/collapse.directive';
-import { IconSmileComponent } from '../../icons/icon-smile/icon-smile.component';
-import { environment } from '../../../environments/environment';
 import { NgClass } from '@angular/common';
 import { NameWbrPipe } from '../../components/name-wbr.pipe';
 
 @Component({
   selector: 'app-index',
-  imports: [RouterLink, ChipComponent, IconCaretComponent, CollapseDirective, IconSmileComponent, NgClass, NameWbrPipe],
+  imports: [RouterLink, ChipComponent, IconCaretComponent, CollapseDirective, NgClass, NameWbrPipe],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss',
 })
 export class IndexComponent {
   protected collapsedFilters: Record<string, boolean> = {};
-  protected readonly showFeedback: boolean = environment.feedbackButtons;
   protected readonly filters: Signal<Filter[]> = computed(() => this.appService.filters());
   protected readonly deprecatedElements: Signal<string[]> = computed(() => {
     const elements = this.appService.frankDoc()?.elements;
