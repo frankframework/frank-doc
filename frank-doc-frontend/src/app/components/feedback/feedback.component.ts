d8527f72845c69d2dafb65f75cd3e0f1eb254a83
Sergi Philipsen <philipsen.sergi@gmail.com>
2025-04-29 15:19:15 +0200

    Add info and link to issue

--- frank-doc-frontend/src/app/components/feedback/feedback.component.ts
+++ frank-doc-frontend/src/app/components/feedback/feedback.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { IconDocumentAddComponent } from '../../icons/icon-document-add/icon-document-add.component';
+import { AppService } from '../../app.service';
 
 @Component({
   selector: 'app-feedback',
@@ -7,4 +9,22 @@ import { IconDocumentAddComponent } from '../../icons/icon-document-add/icon-doc
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss',
 })
-export class FeedbackComponent {}
+export class FeedbackComponent {
+  private appService: AppService = inject(AppService);
+  private router: Router = inject(Router);
+
+  onFeedbackClick(): void {
+    const currentPage = this.router.url;
+    const frankDocVersion = this.appService.frankDoc()?.metadata.version ?? 'unknown';
+
+    const markdown = `
+**Current Page:** \`${currentPage}\`
+**Frank!Doc Version:** \`${frankDocVersion}\`
+`;
+
+    const encodedMarkdown = encodeURIComponent(markdown.trim());
+    const githubUrl = `https://github.com/frankframework/frankframework/issues/new?template=4-documentation.yml&context=${encodedMarkdown}`;
+
+    window.open(githubUrl, '_blank');
+  }
+}
