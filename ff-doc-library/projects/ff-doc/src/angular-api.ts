e14782d25437220aeb7645d3caee316dd9b12e88
Vivy <4380412+Matthbo@users.noreply.github.com>
2025-11-19 19:29:33 +0100

    Remove library code
    New library can be found at @frankframework/doc-library

--- ff-doc-library/projects/ff-doc/src/angular-api.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/angular/ff-doc.angular';
-export * from './lib/angular/javadoc-transform.directive';
--- ff-doc-library/projects/ff-doc/src/es-api.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/frankdoc.types';
-export type * from './lib/ff-doc-base';
-
-export * from './lib/ff-doc';
-export * from './lib/frankdoc.utilities';
