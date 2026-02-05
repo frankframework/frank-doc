69269d0029e31fdce7efc511b28e865989091a5a
Tim van der Leeuw <tim@wearefrank.nl>
2026-02-05 12:53:13 +0100

    Move to JDK25 as minimum baseline for building the FrankDoc

--- frank-doc-doclet/src/test/java/org/frankframework/frankdoc/model/FrankDocModelDocletTest.java
+++ frank-doc-doclet/src/test/java/org/frankframework/frankdoc/model/FrankDocModelDocletTest.java
@@ -1,27 +1,25 @@
 package org.frankframework.frankdoc.model;
 
+import static org.frankframework.frankdoc.Constants.FRANK_DOC_GROUP_VALUES_PACKAGE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.frankframework.frankdoc.wrapper.FrankClassRepository;
 import org.frankframework.frankdoc.wrapper.TestUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-
-import static org.frankframework.frankdoc.Constants.FRANK_DOC_GROUP_VALUES_PACKAGE;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FrankDocModelDocletTest {
 	private static final String SIMPLE = "org.frankframework.frankdoc.testtarget.simple.";
 	private static final String EXPECTED_DESCRIPTION =
 			"The JavaDoc comment of class \"Container\".\n" +
 			"\n" +
-			" This is additional text that we do not add to the XSDs or the Frank!Doc website.";
+			"This is additional text that we do not add to the XSDs or the Frank!Doc website.";
 	private static final String EXPECTED_DESCRIPTION_HEADER = "The JavaDoc comment of class \"Container\".";
 
 	private FrankDocModel instance;
 
 	@BeforeEach
-	public void setUp() throws IOException {
+	public void setUp() {
 		FrankClassRepository repository = TestUtil.getFrankClassRepositoryDoclet(SIMPLE, FRANK_DOC_GROUP_VALUES_PACKAGE);
 		instance = FrankDocModel.populate(TestUtil.resourceAsURL("doc/xsd-element-name-digester-rules.xml"), null, SIMPLE + "Container", repository);
 	}
