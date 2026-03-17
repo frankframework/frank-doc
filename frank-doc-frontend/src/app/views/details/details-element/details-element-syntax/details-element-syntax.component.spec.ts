b6aaef3c4156bed8f1483f35f921f3f91600cc17
Vivy <4380412+Matthbo@users.noreply.github.com>
2026-03-17 13:12:38 +0100

    Update frank-doc-frontend/src/app/views/details/details-element/details-element-syntax/details-element-syntax.component.spec.ts
    Co-authored-by: Erik <dongenevan@gmail.com>

--- frank-doc-frontend/src/app/views/details/details-element/details-element-syntax/details-element-syntax.component.spec.ts
+++ frank-doc-frontend/src/app/views/details/details-element/details-element-syntax/details-element-syntax.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DetailsElementSyntaxComponent } from './details-element-syntax.component';
 
 describe('DetailsElementSyntaxComponent', () => {
