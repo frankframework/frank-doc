b2627206f2a5f963fcbb72ec2e40f6250a2bb2a6
Vivy <4380412+Matthbo@users.noreply.github.com>
2026-05-12 13:15:25 +0200

    Change Manual to Docs & rename application to always be Reference

--- frank-doc-frontend/src/environments/environment.ts
+++ frank-doc-frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   javadocBaseUrl: 'https://javadoc.frankframework.org',
   githubWikiBaseUrl: 'https://github.com/frankframework/frankframework/wiki',
   frankDocUrl: 'js/frankdoc.json',
-  applicationName: 'FF! Doc',
+  applicationName: 'FF! Reference',
   relatedSearchResults: false,
   hideSnapshotVersion: true,
 };
