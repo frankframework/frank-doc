20fa2387ae1fcb859e21743273ba3ab779715619
evandongen <dongenevan@gmail.com>
2026-07-17 14:32:34 +0200

    Add a test for the new use case

---
+++ frank-doc-doclet/src/test/java/org/frankframework/frankdoc/testtarget/examples/parentforward/MiddlePipe.java
@@ -0,0 +1,7 @@
+package org.frankframework.frankdoc.testtarget.examples.parentforward;
+
+import org.frankframework.doc.Forward;
+
+@Forward(name = "success")
+public abstract class MiddlePipe extends AbstractBase {
+}
