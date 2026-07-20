bb16f3f04fcc2e978cb7f4c0440e4af5ed015a19
Vivy <4380412+Matthbo@users.noreply.github.com>
2026-07-20 17:02:10 +0200

    Improve visibility of deprecated attributes

--- frank-doc-frontend/src/app/views/details/details-element/details-element-attributes/details-element-attributes.component.ts
+++ frank-doc-frontend/src/app/views/details/details-element/details-element-attributes/details-element-attributes.component.ts
@@ -13,6 +13,7 @@ import { CollapseDirective } from '../../../../components/collapse.directive';
 import { NameWbrPipe } from '../../../../components/name-wbr.pipe';
 import { IconCaretComponent } from '../../../../icons/icon-caret-down/icon-caret.component';
 import { DEFAULT_RETURN_CHARACTER } from '../../../../app.constants';
+import { IconHelpComponent } from '../../../../icons/icon-help/icon-help.component';
 
 type EnumValueEntry = {
   valueName: string;
@@ -21,7 +22,15 @@ type EnumValueEntry = {
 
 @Component({
   selector: 'app-details-element-attributes',
-  imports: [IconCaretComponent, NgTemplateOutlet, JavadocTransformDirective, NameWbrPipe, NgClass, CollapseDirective],
+  imports: [
+    IconCaretComponent,
+    NgTemplateOutlet,
+    JavadocTransformDirective,
+    NameWbrPipe,
+    NgClass,
+    CollapseDirective,
+    IconHelpComponent,
+  ],
   templateUrl: './details-element-attributes.component.html',
   styleUrl: '../details-element-options.scss',
 })
